36d2ea1cd98de119593ec8b9b9832b54f4a6d2f7
ddorn <diego.dorn@free.fr>
2020-09-27 01:29:00 +0200

    add structures

---
+++ assets/structures/boing.s
@@ -0,0 +1,1 @@
+{"name": "boing", "objs": [{"type": "Bounce", "pos": [0.0, 45.0]}, {"type": "Boost", "pos": [2.0, 32.0]}, {"type": "Boost", "pos": [2.0, 17.0]}, {"type": "Bounce", "pos": [0.0, 0.0]}, {"type": "Bounce", "pos": [60.0, 15.0]}, {"type": "Bounce", "pos": [60.0, 30.0]}, {"type": "Boost", "pos": [62.0, 47.0]}, {"type": "Boost", "pos": [62.0, 2.0]}]}
