a085b05b169fd981bf5878d22474fa0a7b2d2391
ddorn <diego.dorn@free.fr>
2020-09-27 12:16:30 +0200

    fix quit() missing in pyinstaller + warn when modules are not there

--- run_game.py
+++ run_game.py
@@ -2,17 +2,30 @@ import sys
 
 if sys.version_info < (3, 7):
     print("You need at least python 3.7")
-    exit(1)
+    sys.exit(1)
 
 try:
     import pygame
 except ImportError:
     print("You need to have pygame installed")
-    print("Run  pip install pygame  to get it")
-    quit(1)
+    print("Run \033[31m pip install pygame \033[m to get it")
+    sys.exit(1)
 
+try:
+    import click
+except ImportError:
+    print("You need to have click installed")
+    print("Run \033[31m pip install click \033[m to get it")
+    sys.exit(1)
+
+try:
+    import requests
+except ImportError:
+    print("You need to have requests installed")
+    print("Run \033[31m pip install requests \033[m to get it")
+    sys.exit(1)
 
 if __name__ == "__main__":
     from src import the_casta_way
 
-    quit(the_casta_way())
+    sys.exit(the_casta_way())
